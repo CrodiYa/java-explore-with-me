2e13b6007e169f10cd13f56f367d5fac03806aa2
Crodi <Crodis@yandex.ru>
2026-03-01 21:58:59 +0300

    fix: deleted tests for LocalDataTime

--- stats-service/stats-dto/src/test/java/ru/practicum/dto/FormatterTest.java
+++ stats-service/stats-dto/src/test/java/ru/practicum/dto/FormatterTest.java
@@ -21,13 +21,6 @@ class FormatterTest {
     }
 
     @Test
-    void shouldFormatLocalDateTimeToString() {
-        LocalDateTime dateTime = LocalDateTime.of(2025, 3, 15, 14, 30, 0);
-        String expected = "2025-03-15 14:30:00";
-        assertEquals(expected, Formatter.format(dateTime));
-    }
-
-    @Test
     void shouldParseStringToInstant() {
         String text = "2025-03-15 14:30:00";
         Instant expected = LocalDateTime.of(2025, 3, 15, 14, 30, 0)
@@ -36,21 +29,8 @@ class FormatterTest {
     }
 
     @Test
-    void shouldParseStringToLocalDateTime() {
-        String text = "2025-03-15 14:30:00";
-        LocalDateTime expected = LocalDateTime.of(2025, 3, 15, 14, 30, 0);
-        assertEquals(expected, Formatter.toLocalDateTime(text));
-    }
-
-    @Test
     void shouldThrowExceptionWhenParsingInvalidInstantString() {
         String invalidText = "2025/03/15 14:30:00";
         assertThrows(DateTimeParseException.class, () -> Formatter.toInstant(invalidText));
-    }
-
-    @Test
-    void shouldThrowExceptionWhenParsingInvalidLocalDateTimeString() {
-        String invalidText = "2025/03/15 14:30:00";
-        assertThrows(DateTimeParseException.class, () -> Formatter.toLocalDateTime(invalidText));
     }
 }
