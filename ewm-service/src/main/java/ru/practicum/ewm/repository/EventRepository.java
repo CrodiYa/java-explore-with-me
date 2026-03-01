43a14d95c031171e147afb95b609bb69a6b46900
Crodi <Crodis@yandex.ru>
2026-03-01 23:32:11 +0300

    fix: fix checkstyle

--- ewm-service/src/main/java/ru/practicum/ewm/repository/EventRepository.java
+++ ewm-service/src/main/java/ru/practicum/ewm/repository/EventRepository.java
@@ -3,13 +3,9 @@ package ru.practicum.ewm.repository;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import ru.practicum.ewm.model.event.Event;
-import ru.practicum.ewm.model.participation.ParticipationStatus;
-import ru.practicum.ewm.service.event.EventRequestCount;
 
 import java.util.Collection;
-import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Long>, JpaSpecificationExecutor<Event> {
     Event findEntityById(Long eventId);
