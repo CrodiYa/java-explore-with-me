5abdeb99f58fe8bf8e08b60477b74031237ef65f
Crodi <Crodis@yandex.ru>
2026-03-04 12:11:36 +0300

    fix: checkstyle

--- ewm-service/src/test/java/ru/practicum/ewm/controller/priv/PrivateEventControllerTest.java
+++ ewm-service/src/test/java/ru/practicum/ewm/controller/priv/PrivateEventControllerTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
